b5d207dfb7987a3e7a6f165c0f51eff7d04a2bf1
Jqs7 <7@jqs7.com>
2015-09-17 03:33:21 +0800

    code format

--- assets/footer.js
+++ assets/footer.js
@@ -1,16 +1,12 @@
-/**
- * Created by ling on 15-9-16.
- */
-    var footer = '<div class="ui inverted vertical footer segment">'+
-        '<div class="ui center aligned container">'+
-        '<div class="ui stackable inverted divided grid">'+
-        '<div class=" wide column">'+
-        '<div class="ui inverted link list">'+
-        '<a href="https://github.com/jqs7/telegram-chinese-groups" class="item">群组娘的源代码基于 GPL v3 协议发布</a>'+
-        '</div>'+
-        '<div class="ui horizontal inverted small divided link list">'+
-        '© 2015'+
-        '<a class="item" href="https://github.com/jqs7">@Jqs_7</a>'+
-        '<a class="item" href="https://github.com/lingkong7">@jiadaeleven</a></div></div></div></div></div>';
-    $("div:last").after(footer);
-
+var footer = '<div class="ui inverted vertical footer segment">'+
+    '<div class="ui center aligned container">'+
+    '<div class="ui stackable inverted divided grid">'+
+    '<div class=" wide column">'+
+    '<div class="ui inverted link list">'+
+    '<a href="https://github.com/jqs7/telegram-chinese-groups" class="item">群组娘的源代码基于 GPL v3 协议发布</a>'+
+    '</div>'+
+    '<div class="ui horizontal inverted small divided link list">'+
+    '© 2015 '+
+    '<a class="item" href="https://github.com/jqs7">@Jqs_7</a>'+
+    '<a class="item" href="https://github.com/lingkong7">@jiadaeleven</a></div></div></div></div></div>';
+$("div:last").after(footer);
