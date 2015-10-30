abfa91875a94217ef4f557b1b31dcd5223af6958
Jqs7 <7@jqs7.com>
2015-10-30 19:07:26 +0800

    remove assets and add searchbox key enter event

--- html/src/searchbox.vue
+++ html/src/searchbox.vue
@@ -1,3 +1,16 @@
 <template lang='jade'>
-input(type='text')
+input(type='text' @keydown.enter='submit')
 </template>
+
+<script>
+export default{
+  methods:{
+    submit:function(event){
+      var v = event.target.value.trim();
+      if (v !== ''){
+        window.location.href = '/user/' + v;
+      }
+    }
+  }
+}
+</script>
