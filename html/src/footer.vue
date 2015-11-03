dd9c81de6d75174d68d8dccf2cd5af29baabd2d6
Jqs7 <7@jqs7.com>
2015-11-03 17:10:25 +0800

    add a footer

---
+++ html/src/footer.vue
@@ -0,0 +1,17 @@
+<template lang='jade'>
+  .footer.
+    群组娘的源代码基于
+    <a href='https://github.com/jqs7/Jqs7Bot/blob/master/LICENSE'>GPL v3</a>
+    协议发布</br>
+    © 2015 <a href='https://telegram.me/Jqs_7'>@Jqs_7</a>
+</template>
+
+<style lang='stylus'>
+.footer
+  text-align center
+  margin-left 50%
+  transform translate(-50%, 0)
+  padding 10px 0
+  a
+    text-decoration none
+</style>
--- html/src/index.vue
+++ html/src/index.vue
@@ -3,21 +3,31 @@
   h1 群组娘第二舰队活动图
   searchbox
   chart
+footer
+  foot
 </template>
 
 <style lang='stylus'>
 .center
   text-align center
+footer
+  position absolute
+  left 0
+  bottom 0
+  width 100%
+  background #DDE4E9
 </style>
 
 <script>
 import chart from './chart.vue'
 import searchbox from './searchbox.vue'
+import foot from './footer.vue'
 
 export default{
   components: {
+    searchbox,
     chart,
-    searchbox
+    foot
   }
 }
 </script>
